ec3cca9958f7b2d8d1251e2b727beb3c53edb2b1
Juliana Lucca <lucca.editora@gmail.com>
2023-05-09 14:36:15 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Cabecalho from './Components/Cabecalho'
+import Container from './Components/Container'
+
+function App() {
+  return (
+    <>
+      <Cabecalho />
+      <Container />
+    </>
+  )
+}
+
+export default App
